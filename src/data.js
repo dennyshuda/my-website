543f49cd1edddb465b93231bcd239ef3d174dea9
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:39:54 +0700

    feat: create data projects

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = [
+  {
+    id: 1,
+    image:
+      "https://drive.google.com/uc?export=view&id=16TZNwUi58NHt-6cX5yiIW32b9zIcJUTf",
+    title: "Halo Buku Commerce",
+    link: "https://github.com/CrashTeamR/HaloBuku",
+  },
+  {
+    id: 2,
+    image:
+      "https://drive.google.com/uc?export=view&id=1UAiYbhgAdEGJ0IlxL8PhC7aCyuLcxRPF",
+    title: "Kepo Question",
+    link: "https://github.com/CrashTeamR/kepo",
+  },
+];
