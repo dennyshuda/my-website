5558d3b49725ca464f9972563a48e583aacfedf9
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:38:32 +0700

    feat: create contact components

---
+++ src/components/contact.jsx
@@ -0,0 +1,29 @@
+export function Contact() {
+  return (
+    <div className="mt-12">
+      <h1 className="text-2xl font-bold">Contact</h1>
+      <ul className="text-xl">
+        <li>
+          <a href="https://www.linkedin.com/in/dennyshuda" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/dennyshuda" target="_blank">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/dennyshuda" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            Telegram
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
