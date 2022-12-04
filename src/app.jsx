c5386f36c2be814e7f6e3f5b1e998c8d23fffa7c
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:40:15 +0700

    feat: import and render components

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { Header } from "./components/header";
+import { About } from "./components/about";
+import { Capabilities } from "./components/capabilities";
+import { Contact } from "./components/contact";
+import { Projects } from "./components/projects";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <div className="container mx-auto px-10">
+        <Header />
+        <About />
+        <Capabilities />
+        <Contact />
+        <Projects />
+        <Footer />
+      </div>
     </div>
   );
 }
