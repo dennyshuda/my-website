0fdc612ac779f9624b5e33347a20fb964bc4077c
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:38:50 +0700

    feat: create header components

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <div className="mt-40 text-center">
+      <h1 className="text-5xl font-bold mb-5">Denny Setyawan Huda</h1>
+      <h1 className="text-5xl font-bold mb-5">
+        I am a Frontend Developer Enthusiast
+      </h1>
+      <p className="text-2xl mb-5">Keep learning</p>
+    </div>
+  );
+}
