ac530541879478b6f11ce22f4e0f843e15b25f42
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:38:00 +0700

    feat: create about component

---
+++ src/components/about.jsx
@@ -0,0 +1,21 @@
+export function About() {
+  return (
+    <div className="mt-10">
+      <img
+        className="w-40 h-40 md:w-80 md:h-80 object-cover rounded-full mx-auto my-12"
+        src="https://drive.google.com/uc?export=view&id=1T1ifO2YZ2tMxPBPQGtv0hZaq8x1o8REh"
+        alt="Denny Setyawan Huda"
+      />
+      <h1 className="text-2xl font-bold">About me</h1>
+      <p className="text-xl text-justify">
+        Hello! I'm Denny. I am a Frontend Developer based in Indonesia. There
+        are a lot of things and technologies to learn in Frontend Development
+        and I am motivated to learn as much as possible. I enjoy learning
+        something new and getting feedback to make myself better and improve. On
+        this website I will write several blogs and showcase my projects. I
+        believe that by posting my work it is the best way to share my knowledge
+        along the way. So give me a call and I'll be happy to help!
+      </p>
+    </div>
+  );
+}
