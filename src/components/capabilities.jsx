12ffa8ae531c091a95b0139e5f910f4836834a12
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 16:38:16 +0700

    feat: create capabilities components

---
+++ src/components/capabilities.jsx
@@ -0,0 +1,14 @@
+export function Capabilities() {
+  return (
+    <div className="mt-12">
+      <h1 className="text-2xl font-bold">Capabilities</h1>
+      <ul className="text-xl">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Web Development</li>
+        <li>Web Design</li>
+      </ul>
+    </div>
+  );
+}
